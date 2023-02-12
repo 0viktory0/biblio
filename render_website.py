f46bd71b532756aad2b49077f78c67a896aa0ec4
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-02-12 12:47:52 +0300

    fixed site

--- render_website.py
+++ render_website.py
@@ -1,16 +1,15 @@
+import json
 import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
-import json
 from more_itertools import chunked
 from pathlib import Path
 
 
 def on_reload():
     os.makedirs('./pages', exist_ok=True)
-
- #   with open('books.json', 'r', errors="ignore") as file:
-  #      books = json.load(file)
 
     with open("books.json", encoding="utf-8") as my_file:
         books_json = my_file.read()
@@ -21,13 +20,12 @@ def on_reload():
         loader=FileSystemLoader('./'),
         autoescape=select_autoescape(['html', 'xml'])
     )
+
     books_on_pages = 20
     books_pages = list(chunked(books, books_on_pages))
-
     for num, books_on_page in enumerate(books_pages, 1):
         grouped_books = list(chunked(books_on_page, 2))
         template = env.get_template('template.html')
-
         rendered_page = template.render(
             number_of_page=num,
             grouped_books=grouped_books,
@@ -41,16 +39,9 @@ def on_reload():
             file.write(rendered_page)
 
 
-def main():
+if __name__ == '__main__':
     on_reload()
 
     server = Server()
     server.watch('./template.html', on_reload)
-    server.serve(root='.')
-
-
-if __name__ == '__main__':
-    main()
-
-
-
+    server.serve(root='.', default_filename='pages/index1.html')
