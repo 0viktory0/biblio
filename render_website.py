96a46f164554ce3cde39c8ef0594de6dd6b5aa4b
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-02-15 14:46:59 +0300

    fixed code

--- render_website.py
+++ render_website.py
@@ -6,35 +6,40 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
-from pathlib import Path
+from math import ceil
 
 
 def on_reload(json_file_path):
-    os.makedirs('./pages', exist_ok=True)
     path = 'pages'
+    os.makedirs(path, exist_ok=True)
 
-    with open(json_file_path, encoding='utf-8') as my_file:
-        books_description = json.load(my_file)
+    with open(json_file_path, encoding='utf8') as file:
+        books_description = json.load(file)
 
     env = Environment(
-        loader=FileSystemLoader('./'),
+        loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
     cards_on_pages = 20
-    columns = 2
+    rows_on_page = 10
+    number_of_pages = ceil(len(books_description) / cards_on_pages)
     chuncked_books = list(chunked(books_description, cards_on_pages))
-    for num, cards_on_pages in enumerate(chuncked_books, 1):
-        grouped_books = list(chunked(cards_on_pages, columns))
+    last_page = len(chuncked_books) - 1
+    for num, books in enumerate(chuncked_books, 1):
+        grouped_books = list(chunked(books, rows_on_page))
         template = env.get_template('template.html')
+        page_path = os.path.join(
+            path,
+            f'index{num}.html')
+
         rendered_page = template.render(
-            number_of_page=num,
-            grouped_books=grouped_books,
-            pages_count=len(chuncked_books)
+            chuncked_books=grouped_books,
+            number_of_pages=number_of_pages,
+            current_page_number=num,
+            last_page=last_page
         )
-        with open(
-                os.path.join(path, f'index{num}.html'), 'w', encoding='utf8'
-        ) as file:
+        with open(page_path, 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
 
